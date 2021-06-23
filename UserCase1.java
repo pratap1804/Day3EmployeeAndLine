8d3256c0af4e2b3428db366f270acc0489fd46e5
Shubham <pratap.1804@gmail.com>
2021-06-23 22:52:40 +0530

    Added: Random Function To Check For Attendance

---
+++ UserCase1.java
@@ -0,0 +1,12 @@
+public class UserCase1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee wage problem");
+
+        int n =(int)((Math.random()*10)%2);
+        if(n == 0) {
+            System.out.println("Employee is Absent");
+        }
+        else
+                System.out.println("Employee is Present");
+        }
+    }
